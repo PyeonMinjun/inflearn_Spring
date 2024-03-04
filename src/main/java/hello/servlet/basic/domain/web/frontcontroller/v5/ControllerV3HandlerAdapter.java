a03fe4cd0e1522ccbec5896fb25041b68d9aa473
PyeonMinjun <alswns9810@gmail.com>
2024-03-04 23:57:54 +0900

    feat : 유연한 컨트롤러1 - v5

---
+++ src/main/java/hello/servlet/basic/domain/web/frontcontroller/v5/ControllerV3HandlerAdapter.java
@@ -0,0 +1,36 @@
+package hello.servlet.basic.domain.web.frontcontroller.v5;
+
+import hello.servlet.basic.domain.web.frontcontroller.ModelView;
+import hello.servlet.basic.domain.web.frontcontroller.v3.ControllerV3;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
+
+    @Override
+    public boolean supports(Object handler) {
+        return (handler instanceof ControllerV3);
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
+        ControllerV3 controller = (ControllerV3) handler; // 실제 컨트롤러 호출
+        Map<String, String> paramMap = createParamMap(request);
+        ModelView mv = controller.process(paramMap); // 모델 뷰 반환
+        return  mv;
+
+
+    }
+
+    private static Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+    }
+}
---
+++ src/main/java/hello/servlet/basic/domain/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -0,0 +1,78 @@
+package hello.servlet.basic.domain.web.frontcontroller.v5;
+
+import hello.servlet.basic.domain.web.frontcontroller.ModelView;
+import hello.servlet.basic.domain.web.frontcontroller.MyView;
+import hello.servlet.basic.domain.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.basic.domain.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.basic.domain.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV5", urlPatterns = "/front-controller/v5/*")
+public class FrontControllerServletV5 extends HttpServlet {
+
+    private final Map<String, Object> handlerMappingMap = new HashMap<>();
+    private final List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
+
+   public FrontControllerServletV5() {
+       initHandlerMappingMap();
+       initHandlerAdapters();
+   }
+    private void initHandlerMappingMap() {
+        handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+    }
+    private void initHandlerAdapters() {
+        handlerAdapters.add(new ControllerV3HandlerAdapter());
+    }
+
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        MemberFormControllerV3
+       Object handler = getHandler(request); // 핸들러 매핑정보 호 1. 핸들러 조회
+        if (handler == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        //ControllerV3HandlerAdapter
+        MyHandlerAdapter adapter = getHandlerAdapter(handler); // 핸들러 어댑터 찾아오기. 2. 핸들러를 처리할 수 있는 핸들러 어댑터 조회 v3핸들러찾음
+
+        ModelView mv = adapter.handle(request, response, handler); // 핸들러 호출
+
+        String viewName = mv.getViewName();
+        MyView view = viewResolver(viewName);
+        view.render(mv.getModel(), request, response);
+    }
+
+    private Object getHandler(HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        return handlerMappingMap.get(requestURI);
+    }
+
+    private MyHandlerAdapter getHandlerAdapter(Object handler) {
+        for (MyHandlerAdapter adapter : handlerAdapters) { // 모든 핸들러를 뒤진다.
+            if (adapter.supports(handler)) { // v3가 있으면 supports v3 호출
+                return adapter;
+            }
+        }
+        throw new IllegalStateException("handler adapter를 찾을 수 없다. handler="+handler);
+    }
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+
+
+}
+
