790fdbc10e10f286e52485d75ae56ab4d35e8ac5
PyeonMinjun <alswns9810@gmail.com>
2024-03-05 23:37:33 +0900

    build,rename : 파일이동

---
+++ src/main/java/hello/servlet/basic/web/frontcontroller/MyView.java
@@ -0,0 +1,31 @@
+package hello.servlet.basic.web.frontcontroller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class MyView {
+    private String viewPath;
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        modelToRequestAttribute(model, request);  // 모델에 담긴 데이터를 다꺼내서.
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach((key, value) -> request.setAttribute(key,value)); // Request에 담는다. // jsp는 해당 값을 여기서 꺼냄.
+    }
+}
