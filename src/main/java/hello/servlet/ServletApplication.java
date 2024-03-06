758a3a000d0bc2fe302c4711390bc56aa9bf7c2f
PyeonMinjun <alswns9810@gmail.com>
2024-03-06 19:55:08 +0900

    feat : 스프링MVC - 시작하기, RequestMapping

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -1,8 +1,10 @@
 package hello.servlet;
 
+import hello.servlet.basic.web.springmvc.v1.SprintMemberFormControllerV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 
 @ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
@@ -12,4 +14,9 @@ public class ServletApplication {
 		SpringApplication.run(ServletApplication.class, args);
 	}
 
+//
+//	@Bean
+//	SprintMemberFormControllerV1 sprintMemberFormControllerV1() {
+//		return new SprintMemberFormControllerV1();
+//	}
 }
