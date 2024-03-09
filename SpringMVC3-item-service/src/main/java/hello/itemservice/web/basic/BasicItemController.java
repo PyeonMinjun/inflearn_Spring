e612fe6ad36fe18bcc88d9a41806e215ebd237c7
PyeonMinjun <alswns9810@gmail.com>
2024-03-09 18:27:14 +0900

    feat : 상품목록 - 타임리프

---
+++ SpringMVC3-item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,39 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.domain.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+@Controller
+public class BasicItemController {
+
+    private final ItemRepository itemRepository = new ItemRepository();
+
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("testA", 1000, 10));
+        itemRepository.save(new Item("testB", 2000, 20));
+    }
+
+
+}
