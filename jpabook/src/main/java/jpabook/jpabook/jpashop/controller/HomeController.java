b2396864ec872c8003bbe5caeb7c01f86f9ac9c1
PyeonMinjun <alswns9810@gmail.com>
2024-03-26 20:19:11 +0900

    feat : 홈 화면과 레이아웃, 회원등록

---
+++ jpabook/src/main/java/jpabook/jpabook/jpashop/controller/HomeController.java
@@ -0,0 +1,17 @@
+package jpabook.jpabook.jpashop.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+    @RequestMapping("/")
+    private String home() {
+        log.info("home controller");
+        return "home";
+    }
+}
