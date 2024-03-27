c5b8d8938092a82ecfb16dbde2af97bcfe6fd416
PyeonMinjun <alswns9810@gmail.com>
2024-03-27 14:16:34 +0900

    feat : 회원 조회 API

--- jpabook/src/main/java/jpabook/jpabook/jpashop/api/HelloApiController.java
+++ jpabook/src/main/java/jpabook/jpabook/jpashop/api/HelloApiController.java
@@ -14,6 +14,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 import javax.xml.namespace.QName;
+import java.util.List;
+import java.util.stream.Collectors;
 
 //@Controller
 //@ResponseBody
@@ -22,6 +24,49 @@ import javax.xml.namespace.QName;
 @RequiredArgsConstructor
 public class HelloApiController {
     private final MemberService memberService;
+
+    /**
+     * 조회 V1 : 응답 값으로 엔티티를 직접 외부에 노출한다.
+     * 문제점
+     * - 엔티티에  프레젠테이션 계층을 위한 로직이 추가된다.
+     *  - 기본적으로 엔티티의 모든 값이 노출된다.
+     *  - 응답 스펙을 맞추기 위해 로직이 추가된다. (@JsonIgnore, 별도의 뷰 로직 등등)
+     *  - 실무에서는 같은 엔티티에 대해 API가 용도에 따라 다양하게 만들어지는데, 한 엔티티에 각각의
+     *  API를 위한 프레젠테이션 응답 로직을 담기는 어렵다.
+     *  - 엔티티가 변경되면 API 스펙이 변한다.
+     *  -- 추가로 컬렉션을 직접 반환하면 향후 API 스펙을 변경하기 어렵다. (별도의 Result 클래스 생성으로 해결)
+     *  결론
+     *  - API 응답 스펙에 맞추어 별도의 DTO를 반환한다.
+     */
+    //조회 V1: 안 좋은 버전, 모든 엔티티가 노출, @JsonIgnore -> 이건 정말 최악, api가 이거 하나인가! 화면에 종속적이지 마라!
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(),collect);
+    }
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
+
 
     /**
      * 등록 v1 : 요청값으로 Member 엔티티를 직접 받는다.
@@ -36,6 +81,9 @@ public class HelloApiController {
 
     @PostMapping("/api/v1/members")
     private CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) { //엔티티를 파라미터로 받지말자.
+
+
+
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
@@ -57,6 +105,9 @@ public class HelloApiController {
         return new CreateMemberResponse(id);
     }
 
+    /**
+     * 수정
+     */
     @PutMapping("/api/v2/members/{id}")
     private UpdateMemberResponse updateMemberV2(
             @PathVariable("id") Long id,
