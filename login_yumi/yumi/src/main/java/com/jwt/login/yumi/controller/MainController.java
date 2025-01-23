9316e0014ef64684e4a7c47343fc89ec449c4c6a
PyeonMinjun <alswns9810@gmail.com>
2025-01-23 20:50:45 +0900

    build, feat : 프로젝트 기본 생성 및 의존성 설정, controller 추가

---
+++ login_yumi/yumi/src/main/java/com/jwt/login/yumi/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.jwt.login.yumi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class MainController {
+
+    @GetMapping("/")
+    public String MainController() {
+        return "main controller";
+
+    }
+
+}
