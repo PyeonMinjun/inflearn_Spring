9dd65b258b7618f2bb8688806ca891748e42c551
PyeonMinjun <alswns9810@gmail.com>
2025-01-26 07:18:56 +0900

    feat : 세션정보 사용

--- login_yumi/yumi/src/main/java/com/jwt/login/yumi/controller/MainController.java
+++ login_yumi/yumi/src/main/java/com/jwt/login/yumi/controller/MainController.java
@@ -1,9 +1,15 @@
 package com.jwt.login.yumi.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 @ResponseBody
@@ -11,7 +17,24 @@ public class MainController {
 
     @GetMapping("/")
     public String MainController() {
-        return "main controller";
+
+        /**
+         * jwt도 stateless이지만 일시적으로 내부에 세션을 만들어서 사용하기 때문에 name, role값을 가져와서 사용할 수 있다.
+         */
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+
+        return "main controller" + name  + role;
+
+
+
 
     }
 
