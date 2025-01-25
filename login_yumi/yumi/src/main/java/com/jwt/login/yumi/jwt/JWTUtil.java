d0862e732186dd7101abe8d7981cd3f6ed478982
PyeonMinjun <alswns9810@gmail.com>
2025-01-25 11:24:30 +0900

    feat : JWT 발급 및 검증 클래

---
+++ login_yumi/yumi/src/main/java/com/jwt/login/yumi/jwt/JWTUtil.java
@@ -0,0 +1,57 @@
+package com.jwt.login.yumi.jwt;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+
+    private SecretKey  secretKey;
+    public JWTUtil(@Value("${spring.jwt.secret}") String secret) {
+
+        // 시크릿키 암호화
+        secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    // 검증
+    public String getUsername(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseEncryptedClaims(token).getPayload().get("username", String.class);
+    }
+
+    public String getRole(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
+    }
+    // 만료 날짜 확인
+    public Boolean isExpired(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+    // jwt 생성
+    public String createJwt(String username, String role, Long expiredMs) {
+        return Jwts.builder()
+                .claim("username", username)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
