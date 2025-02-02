c98e4975ccddbad6b60476f34a8315b6644c1085
PyeonMinjun <alswns9810@gmail.com>
2025-02-02 10:33:43 +0900

    feat : access 토큰 필터
    JwtFilter 수정

--- login_yumi/yumi/src/main/java/com/jwt/login/yumi/jwt/JwtFilter.java
+++ login_yumi/yumi/src/main/java/com/jwt/login/yumi/jwt/JwtFilter.java
@@ -2,6 +2,7 @@ package com.jwt.login.yumi.jwt;
 
 import com.jwt.login.yumi.dto.CustomUserDetails;
 import com.jwt.login.yumi.entity.UserEntity;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,10 +10,10 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.http.SecurityHeaders;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class JwtFilter extends OncePerRequestFilter  {
 
@@ -26,51 +27,50 @@ public class JwtFilter extends OncePerRequestFilter  {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        // request에서 Authorization 헤더를 찾음
-        String authorization = request.getHeader("Authorization");
-//
-//        System.out.println("모든 헤더 출력:");
-//        java.util.Enumeration<String> headerNames = request.getHeaderNames();
-//        while (headerNames.hasMoreElements()) {
-//            String headerName = headerNames.nextElement();
-//            System.out.println(headerName + ": " + request.getHeader(headerName));
-//        }
+        // 헤더에서 access키에 담긴 토큰을 꺼냄
+        String accessToken = request.getHeader("access");
 
-
-
-
-        //Authorization 헤더 검증
-        if (authorization == null || !authorization.startsWith("Bearer ")) {
-            System.out.println("token null: " + authorization);
+        //토큰이 없다면 다음 필터로 넘김
+        if (accessToken == null) {
             filterChain.doFilter(request,response);
-            // 조건 해당되면 메소드 종료(필수)
+        }
+
+        //토큰 만료 여부 확인, 만료시 다음 필터로 넘기지 않음
+        try {
+            jwtUtil.isExpired(accessToken);
+        } catch (ExpiredJwtException e) {
+
+            //response body
+            PrintWriter writer = response.getWriter();
+            writer.print("access token expired");
+
+            //response status code
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
             return;
         }
 
-        System.out.println("authorization now");
-        // Bearer 부분 제거후 순수 토큰만 획득
-        String token = authorization.split(" ")[1];
+        // 토큰이 access인지(발급시 페이로드에 명시)
+        String category = jwtUtil.getCategory(accessToken);
 
+        if (!category.equals("access")) {
 
-        // 토큰 소멸시간 검증
-        if (jwtUtil.isExpired(token)){
-            System.out.println("token expired");
-            filterChain.doFilter(request, response);
+            //response body
+            PrintWriter writer = response.getWriter();
+            writer.print("invalid access token");
 
-            // 조건이 해당되면 메소드 종료 (필수)
+            //response status code
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return;
         }
+
         // 토큰에서 username과 role 획득
-        String username = jwtUtil.getUsername(token);
-        String role = jwtUtil.getRole(token);
+        String username = jwtUtil.getUsername(accessToken);
+        String role = jwtUtil.getRole(accessToken);
 
         //userEntity를 생성하여 값 set
         UserEntity userEntity = new UserEntity();
         userEntity.setUsername(username);
-        userEntity.setPassword("tmppassword");
-        System.out.println("Extracted Role: " + role);
         userEntity.setRole(role);
-        System.out.println("Set Role: " + userEntity.getRole());
 
         // 유저디테일에 회원 정보 객체 담기
         CustomUserDetails customUserDetails = new CustomUserDetails(userEntity);
@@ -78,7 +78,7 @@ public class JwtFilter extends OncePerRequestFilter  {
         // 스프링 시큐리티 인증 토큰 생성
         Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
 
-        // 세션에 사용자 등록
+        //일시적 세션에 사용자 등록하여 인증 완료
         SecurityContextHolder.getContext().setAuthentication(authToken);
         System.out.println("인증 완료: " + authToken.getAuthorities());
 
