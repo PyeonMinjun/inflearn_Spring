49953b2510d9de2904be2f233e37c8c941ffffa9
PyeonMinjun <alswns9810@gmail.com>
2025-02-26 21:38:04 +0900

    feat : security config 작성

---
+++ OAuthJWT/src/main/java/com/yumi/OAuthJWT/config/securityConfig.java
@@ -0,0 +1,46 @@
+package com.yumi.OAuthJWT.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class securityConfig {
+
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+
+    // csrf disable
+    http
+        .csrf((auth) -> auth.disable());
+
+    // form 로그인방식 disable
+    http
+        .formLogin((auth) -> auth.disable());
+
+    // HTTP Basic 방식 disable
+    http
+        .httpBasic((auth) -> auth.disable());
+
+    // oauth2 설정
+    http
+        .oauth2Login(Customizer.withDefaults());
+
+    // 경로별 인가작업
+    http
+        .authorizeHttpRequests((auth) -> auth
+            .requestMatchers("/").permitAll()
+            .anyRequest().authenticated());
+
+    // 세션 stateless
+    http
+        .sessionManagement((session) -> session
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+    return http.build();
+  }
+}
